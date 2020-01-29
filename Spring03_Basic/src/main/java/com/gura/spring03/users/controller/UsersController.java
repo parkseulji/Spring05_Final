7f57eb194f22ba5d82d3bc3d337361f52558d26f
parkseulji <parkseulji112@gmail.com>
2020-01-29 14:57:19 +0900

    Spring03- 로그인 기능 추가

--- Spring03_Basic/src/main/java/com/gura/spring03/users/controller/UsersController.java
+++ Spring03_Basic/src/main/java/com/gura/spring03/users/controller/UsersController.java
@@ -1,11 +1,17 @@
 package com.gura.spring03.users.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UsersController {
+	//
 
+	//로그인 폼 요청처리
 	@RequestMapping("/users/loginform")
 	public String loginform() {
 		//수행 할 비즈니스 로직은 없다.
@@ -13,5 +19,23 @@ public class UsersController {
 		
 		//view page의 정보만 단순히 리턴 해 주는 경우도 있다.
 		return "users/loginform";
+	}
+	//로그인 요청처리
+	@RequestMapping("/users/login")
+	public String login(HttpServletRequest request, HttpSession session) {
+		//폼 전송되는 파라미터를 추출한다.
+		String id=request.getParameter("id");
+		String pwd=request.getParameter("pwd");
+		//유효한 정보인지 여부
+		boolean isSuccess=false;
+		if(id.equals("gura") && pwd.equals("1234")) {
+			isSuccess=true;
+			//로그인 처리를 한다.
+			session.setAttribute("id", id);
+		}
+		//로그인 성공 여부를 request에 담는다.
+		request.setAttribute("isSuccess", isSuccess);
+		//view page의 정보를 리턴한다.
+		return "users/login";
 	}
 }
