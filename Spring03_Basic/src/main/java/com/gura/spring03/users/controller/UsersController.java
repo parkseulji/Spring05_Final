7239c24369132b0ef1fac1a8c477f9fb92ca5053
parkseulji <parkseulji112@gmail.com>
2020-01-29 15:16:12 +0900

    Spring03-로그인 요청처리 2번째 방법 구현

--- Spring03_Basic/src/main/java/com/gura/spring03/users/controller/UsersController.java
+++ Spring03_Basic/src/main/java/com/gura/spring03/users/controller/UsersController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -37,5 +38,26 @@ public class UsersController {
 		request.setAttribute("isSuccess", isSuccess);
 		//view page의 정보를 리턴한다.
 		return "users/login";
+	}
+	/*
+	 * @RequestParam 어노테이션은 전송되는 파라미터를 자동으로 추출 할 때 사용한다.
+	 */	
+	@RequestMapping("/users/login2")
+	public ModelAndView login2(@RequestParam String id, 
+			@RequestParam String pwd,HttpSession session, 
+			ModelAndView mView) {
+		//유효한 정보인지 여부
+		boolean isSuccess=false;
+		if(id.equals("gura") && pwd.equals("1234")) {
+			isSuccess=true;
+			//로그인 처리를 한다.
+			session.setAttribute("id", id);
+	}
+		//view 페이지에서 필요한 모델을 담고
+		mView.addObject("isSuccess", isSuccess);
+		//view page의 정보도 담고
+		mView.setViewName("users/login");
+		//리턴 해 준다.
+		return mView;
 	}
 }
