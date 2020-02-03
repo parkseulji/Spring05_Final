0e812d6234961d6aca0c99308ce874f3cf314def
parkseulji <parkseulji112@gmail.com>
2020-02-03 11:13:45 +0900

    Spring05-MemberServiceImpl객체이용해서 구현까지/ Spring00-mytest 설정완료

--- Spring05_final/src/main/java/com/gura/spring05/member/controller/MemberController.java
+++ Spring05_final/src/main/java/com/gura/spring05/member/controller/MemberController.java
@@ -11,28 +11,29 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.gura.spring05.member.dao.MemberDao;
 import com.gura.spring05.member.dto.MemberDto;
+import com.gura.spring05.member.service.MemberService;
 
 @Controller
 public class MemberController {
 	//의존 객체 주입받기(DI)
 	@Autowired
 	private MemberDao dao;
+	@Autowired
+	private MemberService service;
 	
 	//회원목록 보기 요청(/member/list.do)을 처리 할 컨트롤러의 메소드
 	@RequestMapping("/member/list")
 	public ModelAndView list(ModelAndView mView) {
+		//MemberServiceImpl 객체를 이용해서 비즈니스 로직 처리
 		//회원목록을 얻어오려면?
-		List<MemberDto> list=dao.getList();
-		
-		mView.addObject("list", list); //("key값", type)	
+		service.getList(mView);
 		mView.setViewName("member/list"); //forward이동-> /WEB_INF/views/member/list.jsp
-		return mView;
+		return mView; //Model 과 view page정보가 담긴 객체를 리턴 해 준다.
 	}
 	@RequestMapping("/member/delete")
 	//회원정보 삭제 요청 처리
 	public String delete(@RequestParam int num) {
-		//MemberDao 객체를 이용해서 회원정보 삭제
-		dao.delete(num);
+		service.deleteMember(num);
 		//리다이렉트 응답(자동으로 재요청)
 		return "redirect:/member/list.do";
 	}
@@ -51,7 +52,8 @@ public class MemberController {
 	@RequestMapping("/member/insert")
 	public ModelAndView insert(@ModelAttribute("dto") MemberDto dto,
 			ModelAndView mView) {
-		dao.insert(dto);
+		//서비스를 통해서 비즈니스 로직 처리
+		service.addMember(dto);
 		/*
 		 * @ModelAttribute("dto") MemberDto dto의 의미는
 		 * 1. 전송되는 파라미터를 자동으로 추출해서 MemberDto에 담아 주기도 하고
@@ -59,6 +61,23 @@ public class MemberController {
 		 */
 		//mView.addObject("dto", dto);
 		mView.setViewName("member/insert");
+		return mView;
+	}
+	@RequestMapping("/member/updateform")
+	public ModelAndView updateform(@RequestParam int num,
+			ModelAndView mView) {
+		//ModelAndView 객체에 회원정보가 담기도록 서비스의 메소드 호출
+		service.getMember(mView, num);
+		//view page로 forward 이동해서 수정 할 회원의 정보를 출력 해 준다.
+		mView.setViewName("member/updateform");
+		return mView;
+	}
+	@RequestMapping("/member/update")
+	public ModelAndView update(@ModelAttribute("dto") MemberDto dto,
+			ModelAndView mView) {
+		//회원정보가 수정 되도록 서비스의 메소드 호출
+		service.updateMember(dto);
+		mView.setViewName("member/update");
 		return mView;
 	}
 }
--- Spring05_final/src/main/java/com/gura/spring05/member/dao/MemberDaoImpl.java
+++ Spring05_final/src/main/java/com/gura/spring05/member/dao/MemberDaoImpl.java
@@ -32,4 +32,16 @@ public class MemberDaoImpl implements MemberDao{
 		session.insert("member.insert", dto);
 	}
 
+	@Override
+	public MemberDto getData(int num) {
+		MemberDto dto=session.selectOne("member.getData", num);
+		return dto;
+	}
+
+	@Override
+	public void update(MemberDto dto) {
+		session.update("member.update", dto);
+		
+	}
+
 }
