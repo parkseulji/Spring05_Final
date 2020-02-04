73b0fcc8d6890a57ab25ead84480495a08b306e6
parkseulji <parkseulji112@gmail.com>
2020-02-04 11:23:13 +0900

    Spring05-login 요청 처리

--- Spring05_final/src/main/java/com/gura/spring05/users/service/UsersServiceImpl.java
+++ Spring05_final/src/main/java/com/gura/spring05/users/service/UsersServiceImpl.java
@@ -3,9 +3,13 @@ package com.gura.spring05.users.service;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.gura.spring05.users.dao.UsersDao;
 import com.gura.spring05.users.dto.UsersDto;
@@ -32,6 +36,19 @@ public class UsersServiceImpl implements UsersService{
 		//UsersDao 객체를 이용해서 DB에 저장하기
 		dao.insert(dto);		
 	}
-	
-
+	@Override
+	public void validUser(UsersDto dto, HttpSession session, ModelAndView mView) {
+		//아이디 비밀번호가 유효한지 여부
+		boolean isValid=false;
+		//아이디를 이용해서 저장 된 비밀번호를 읽어온다.
+		String pwdHash=dao.getPwdHash(dto.getId());
+		if(pwdHash != null) {//비밀번호가 존재하고
+			//입력한 비밀번호와 일치하면 로그인 성공
+			isValid=BCrypt.checkpw(dto.getPwd(), pwdHash);
+		}
+		if(isValid) {
+			//로그인 처리
+			session.setAttribute("id", dto.getId());			
+		}		
+	}
 }
